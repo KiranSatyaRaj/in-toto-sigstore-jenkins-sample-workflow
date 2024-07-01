4d9b2b336f6dfb79512033418d48c837c9e8623a
KiranSatyaRaj <kiranjamy20021508@gmail.com>
2024-07-01 23:58:39 +0530

    add in-toto dependency

--- src/main/java/dev/Main.java
+++ src/main/java/dev/Main.java
@@ -1,11 +1,13 @@
 package dev;
 
+import com.google.gson.JsonObject;
 import dev.sigstoreClient.signArtifact;
 public class Main {
     public static void main(String[] args) {
         signArtifact test = new signArtifact("/home/regulusnemea/foo.py");
         test.initSigner();
         test.sign();
-        System.out.println(test.result.toString());
+        String metadata = test.result.toJson();
+        System.out.println(metadata);
     }
 }
